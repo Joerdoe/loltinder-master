22bcf81644278eef51e4703ff806d1e15e252fb2
Joerdoe <jordienijhuis44@gmail.com>
2014-08-14 17:47:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 /**
-  * Requirements: node.js
-  * 1. Install node.js
-  * 2. Open Terminal or Command Prompt
-  * 3. cd to directory where app.js is
-  * 4. Run 'npm install'
-  * 5. GET FACEBOOK NUMERIC ID FROM: http://findmyfacebookid.com/
-  * 6. GET ACCESS TOKEN FROM THE FOLLOWING URL. IT REDIRECTS TO ANOTHER PAGE QUICKLY, SO GRAB THE URL UP TOP BEFORE IT CHANGES QUICKLY.
+  * Wat je nodig hebt: node.js
+  * 1. Installeer node.js
+  * 2. Open je 'Terminal' of 'Command Prompt'
+  * 3. Navigeer met hehulp van het commando 'cd' naar de bestandslocatie van app.js
+  * 4. Voer het commando 'npm install' uit
+  * 5. Verkrijg je numerieke Facebook ID: http://findmyfacebookid.com/
+  * 6. Verkrijg je Access Token via onderstaande url. LET OP! De url in de adres balk verandert snel, dus kopieer de url snel naar je klembord
   *
   * https://www.facebook.com/dialog/oauth?client_id=464891386855067&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=basic_info,email,public_profile,user_about_me,user_activities,user_birthday,user_education_history,user_friends,user_interests,user_likes,user_location,user_photos,user_relationship_details&response_type=token 
   *
-  * 7. To run, type 'node app.js FACEBOOK_ID THE_URL_YOU_COPIED_AND_PASTED'
+  * 7. Om de applicatie te draaien, voer je het volgende commando uit:'node app.js JE_FACEBOOK_ID DE_URL_DIE_JE_GEKOPIEERD_EN_GEPLAKT_HEBT'
   *
-  * Example: node app.js 305102211 "https://www.facebook.com/connect/login_success.html#access_token=CAAGm0PX4ZCpsc3npjYuZASBBIwGz8rS1aQcnjn2cThwpwVVJ9QNqlejhICzkharwvX56IIw5hEsebHIaxSXAs4RvoSBLhsCaKTgQAZBGny1EICA7orRtiuhHVjspFXZBs2GT6JpHUKtlAZD&expires_in=6902"
+  * Voorbeeld: node app.js 305102211 "https://www.facebook.com/connect/login_success.html#access_token=CAAGm0PX4ZCpsc3npjYuZASBBIwGz8rS1aQcnjn2cThwpwVVJ9QNqlejhICzkharwvX56IIw5hEsebHIaxSXAs4RvoSBLhsCaKTgQAZBGny1EICA7orRtiuhHVjspFXZBs2GT6JpHUKtlAZD&expires_in=6902"
   */
 
 var tinderbot = require('tinderbot');
@@ -39,13 +39,13 @@ var interval;
 bot.client.authorize(bot.access_token, bot.facebook_id, function(){
   interval = setInterval(function() {
     if (!bot.client.isAuthorized) {
-      console.log('Token is invalid. Please get another.');
+      console.log('Je Token klopt niet. Probeer het nog een keer.');
       clearInterval(interval);
     }
 
     bot.client.getRecommendations(10, function(error, data){
       if (data && data.message === "recs timeout") {
-        console.log('Recommendations timeout');
+        console.log('Geen mensen meer om te liken..');
         return;
       }
 
@@ -54,15 +54,15 @@ bot.client.authorize(bot.access_token, bot.facebook_id, function(){
         console.log(v.name);
         bot.client.like(v._id, function(error, data) {
           count++;
-          console.log('Count: ' + count);
+          console.log('Aantal: ' + count);
           if (data && data.matched) {
-            console.log('MATCHED!');
+            console.log('JAHAAAAAA! Een match!');
             console.log(user);
           }
         //   if (data.matched) {
         //     bot.client.sendMessage(
         //       id,
-        //       "You're gorgeous. Let's adventure"
+        //       "Hee leukerd!"
         //     );
         //   }
         });
